07326ca4a0fbb37076d373ef3aa7786673680784
Monisha Ranjan <ranjan.monisha233@gmail.com>
2024-04-13 16:41:36 -0400

    Hiding the add row and add column button

---
+++ index.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+  // Initially we are  hiding the add column and add row buttons
+  document.getElementById('b2').style.display = 'none';
+  document.getElementById('b3').style.display = 'none';
+};
